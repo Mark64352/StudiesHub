a79278d8adeee5d73d789b5077b0cf81a9dd591c
Mark64352 <mark.aldea@outlook.com>
2025-09-02 09:57:37 +0300

    testing

---
+++ lesson2.py
@@ -0,0 +1,16 @@
+print("Hello Work!")
+print('Hello Work!')
+print('"Hello Work!", said Joe')
+name = "MARK"
+name = "Mark"
+print(name)
+NAME = "Jenny"
+print(NAME)
+Name = "Jenny"
+print(Name)
+print(name)
+print(name,Name)
+import keyword
+print(keyword.kwlist)
+fullname ="Mark"
+print(fullname)
