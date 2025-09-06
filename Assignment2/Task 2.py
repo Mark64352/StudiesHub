c7e015e95eb9cef24182361a4a19386993ac4c0b
marka <mark.aldea@metropolia.fi>
2025-09-06 15:42:09 +0300

    commit

---
+++ Assignment2/Task 2.py
@@ -0,0 +1,4 @@
+print("What is the radius of a circle in centimeters?")
+radius = float(input("Enter radius of a circle in centimeters:"))
+area = radius ** 2 * 3.141592653589793
+print("The area of the circle is", str(round(area, 2)), "cm^2.") #print(f"The area of the circle is {area:.2f} cm^2.")
