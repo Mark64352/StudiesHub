91ab6656ddbe27f2542020efc094585a0aecd5bd
marka <mark.aldea@metropolia.fi>
2025-09-06 16:52:51 +0300

    commit

---
+++ Assignment2/Task 3.py
@@ -0,0 +1,14 @@
+#input
+print(f"Please enter the length and the width of a rectangle.")
+length = float(input("Enter length of the rectangle:"))
+width = float(input("Enter width of the rectangle:"))
+print()
+
+#calculations
+perimeter = 2 * (length + width)
+area = length * width
+
+#output
+print(f"The total perimeter of the rectangle is {perimeter}.")
+print(f"The area of the rectangle is {area}.")
+
