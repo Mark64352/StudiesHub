9fc6f8578b3fb0c95053d021239ffbedcbfb6328
marka <mark.aldea@metropolia.fi>
2025-09-15 19:07:35 +0300

    commit

---
+++ Assignment 3/Task 2.py
@@ -0,0 +1,32 @@
+print(
+    f"Welcome aboard to Azure Symphony Cruise Ship!\n"
+    f"To assist you to your cabin, could you please provide us your cabin class."
+)
+
+print()
+
+cabin_class = input("What is your cabin class?: ").lower()
+
+if cabin_class == "LUX".lower():
+    print(
+        f"Welcome aboard to Lux cabin!\n"
+        f"You will have an upper-deck cabin with a balcony."
+    )
+elif cabin_class == "A".lower():
+    print(
+        f"Welcome aboard to A- cabin!\n" 
+        f"Your cabin will be above the car deck with a window."
+    )
+elif cabin_class == "B".lower():
+    print(
+        f"Welcome aboard to B- cabin!\n"
+        f"Your cabin will be above the car deck without a window."
+    )
+elif cabin_class == "C".lower():
+    print(
+        f"Welcome aboard to C- cabin!\n"
+        f"Your cabin will be below the car deck without a window."
+    )
+
+else:
+    print(f"\033[1;31;47mINVALID CABIN CLASS\033[0m")
